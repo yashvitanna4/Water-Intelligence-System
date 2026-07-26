3f6573c1058143840d97164e4a4f91f8f4c5fee7
yashvitanna4 <yashvitanna4@gmail.com>
2026-07-26 17:51:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -471,9 +471,11 @@ elif menu == "Water Consumption":
     st.markdown("**Output:** Predicted Water Consumption (MLD)")
 
     @st.cache_resource
-    def load_consumption_model():
-        return joblib.load("Consumption.pkl")
-    consumption_model = load_consumption_model()
+def load_consumption_model():
+    with gzip.open("Consumption.pkl.gz", "rb") as f:
+        return pickle.load(f)
+
+consumption_model = load_consumption_model()
     
     st.subheader(" Predict Water Consumption")
 
